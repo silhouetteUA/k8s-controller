6dbc150ba5777b4d8d412e16ecc61fe1b5989e38
Yevhenii Dmytriiev <dmytriiev.yevhenii@gmail.com>
2025-07-06 15:35:46 -0400

    add more annotations(markers) to kubebuilder such as state and resource related one

--- pkg/api/frontend/v1alpha1/resource.go
+++ pkg/api/frontend/v1alpha1/resource.go
@@ -12,6 +12,8 @@ type FrontendPageSpec struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:shortName=fp,singular=frontendpage,path=frontendpages,scope=Namespaced
 type FrontendPage struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
